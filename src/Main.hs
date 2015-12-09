47038ba46d24fcc1c35089920489f22686d185e0
jvican <jorgevc@fastmail.es>
2015-12-09 11:49:33 +0100

    bdds: share subtrees, last optimization

--- src/Main.hs
+++ src/Main.hs
@@ -4,14 +4,8 @@ import BDDs
 
 main = do
            print $ buildBDD b1 []
-           print $ restrict b2 1 False
-           print $ restrict b2 1 True
            print $ buildBDD b2 [1,2]
            print $ buildBDD b3 [1]
-           print $ restrict (Or(IdRef 2)(Not(IdRef 3))) 2 False
-           print $ restrict (Or(IdRef 2)(Not(IdRef 3))) 2 True
-           print $ buildBDD (Or(IdRef 2)(Not(IdRef 3))) [2, 3]
-           print $ buildBDD (Not(IdRef 3)) [3]
            print $ buildBDD b4 [7,2,3]
            print $ buildBDD b5 [7,2,3]
            print $ buildBDD b6 [1,2,3,4]
--- src/bdds/BDDs.hs
+++ src/bdds/BDDs.hs
@@ -1,5 +1,7 @@
 module BDDs where
 
+import Data.List (sortBy, delete)
+
 type Index = Int
 type Env = [(Index, Bool)]
 
@@ -25,7 +27,7 @@ checkSat (rootId, nodes) e  =
         eval id = eval $ nextNode (lookUp id nodes)
         nextNode :: (Index, NodeId, NodeId) -> NodeId
         nextNode (ind, il, ir)  =
-            if(lookUp ind e) then ir else il
+            if lookUp ind e then ir else il
 
 sat :: BDD -> [Env]
 sat (rootId, nodes) =
@@ -41,27 +43,27 @@ sat (rootId, nodes) =
             in addEnv l (i, False) ++ addEnv r (i, True)
         addEnv :: [Env] -> (Index, Bool) -> [Env]
         addEnv [] x = []
-        addEnv (xs:xxs) x = (x:xs):xxs
+        addEnv (xs:xss) x = (x:xs):xss
 
 -- One step evaluation - including shortcircuits
 simplify :: BExp -> BExp
-simplify (And(Var x)(r)) =  if(x) then r else Var False
-simplify (And(l)(Var x)) = if(x) then l else Var False
-simplify (Or(Var x)(r)) =  if(x) then Var True else r
-simplify (Or(l)(Var x)) = if(x) then Var True else l
+simplify (And (Var x) r) =  if x then r else Var False
+simplify (And l (Var x)) = if x then l else Var False
+simplify (Or (Var x) r) =  if x then Var True else r
+simplify (Or l (Var x)) = if x then Var True else l
 simplify (Not(Var x)) = Var(not x)
 simplify b = b
 
 -- Shannon Boolean restriction
 restrict :: BExp -> Index -> Bool -> BExp
-restrict ref@(IdRef j) i x = if(i == j) then Var x else ref
-restrict (And(l)(r)) i x = simplify $ And(restrict l i x)(restrict r i x)
-restrict (Or(l)(r)) i x = simplify $ Or(restrict l i x)(restrict r i x)
-restrict (Not(e)) i x = simplify $ Not(restrict e i x)
+restrict ref@(IdRef j) i x = if i == j then Var x else ref
+restrict (And l r) i x = simplify $ And(restrict l i x)(restrict r i x)
+restrict (Or l r) i x = simplify $ Or(restrict l i x)(restrict r i x)
+restrict (Not e) i x = simplify $ Not(restrict e i x)
 restrict e i x = e
 
 buildBDD :: BExp -> [Index] -> BDD
-buildBDD e xs = buildBDD' (simplify e) 2 xs
+buildBDD e = buildBDD' (simplify e) 2
 
 buildBDD' :: BExp -> NodeId -> [Index] -> BDD
 buildBDD' (Var False) id xs = (0, [])
@@ -70,19 +72,9 @@ buildBDD' (IdRef i) id xs = (id, [(id, (i, 0, 1))])
 buildBDD' (Not e) id xs@(_:_) =
     let (id', nodes) = buildBDD' e id xs
     in (id', negate' nodes)
-buildBDD' o@(Or(l)(r)) id xs@(_:_) = branch o l r id xs
-buildBDD' a@(And(l)(r)) id xs@(_:_) = branch a l r id xs
-buildBDD' e id [] = error("BExp malformed")
-
-branch :: BExp -> BExp -> BExp -> NodeId -> [Index] -> BDD
-branch e l r id (x:xs) =
-    let n = 2 * id
-        el = restrict e x False
-        er = restrict e x True
-        (lid, ls) = buildBDD' el n xs
-        (rid, rs) = buildBDD' er (n+1) xs
-        node = (id, (x, lid, rid))
-    in (id, node:(ls ++ rs))
+buildBDD' o@(Or l r) id xs@(_:_) = branch o l r id (skip o xs)
+buildBDD' a@(And l r) id xs@(_:_) = branch a l r id (skip a xs)
+buildBDD' e id [] = error "BExp malformed"
 
 not' :: NodeId -> NodeId
 not' 0 = 1
@@ -93,7 +85,86 @@ negate' :: [BDDNode] -> [BDDNode]
 negate' [] = []
 negate' ((id, (ind, x, y)):xs) = (id, (ind, not' x, not' y)):negate' xs
 
--- Testing
+branch :: BExp -> BExp -> BExp -> NodeId -> [Index] -> BDD
+branch e l r id inds@(x:xs) =
+    let n = 2 * id
+        el = restrict e x False
+        er = restrict e x True
+        (lid, ls) = buildBDD' el n xs
+        (rid, rs) = buildBDD' er (n+1) xs
+        node = (id, (x, lid, rid))
+    -- Presence of parent node is necessary for some 
+    -- cases in subtreesConcat. Hence, add it to ls.
+    in (id, subtreesConcat (node:ls) rs)
+
+-- Skip useless indices that don't change the BExp
+skip :: BExp -> [Index] -> [Index]
+skip e [] = []
+skip e l@(x:xs)
+    | e' == e   = skip e xs
+    | otherwise = l
+    where e' = restrict e x False -- use any bool
+
+-- Concat by removing repeated nodes and sharing pointers
+subtreesConcat :: [BDDNode] -> [BDDNode] -> [BDDNode]
+subtreesConcat xs ys = 
+    let xs' = sortBy orderByIndex xs 
+        ys' = sortBy orderByIndex ys
+        zs = sharedNodes xs' ys'
+        toIds :: [(BDDNode, NodeId)] -> [(NodeId, NodeId)]
+        toIds [] = []
+        toIds (((id, _), id2):xs) = (id, id2):toIds xs
+    in shareSubtrees (xs ++ deleteSubtrees ys' zs) (toIds zs)
+
+orderByIndex :: BDDNode -> BDDNode -> Ordering
+orderByIndex (_, (x, _, _)) (_, (y, _, _))
+    | x == y   = EQ
+    | x < y    = LT
+    | x > y    = GT
+
+sharedNodes :: [BDDNode] -> [BDDNode] -> [(BDDNode, NodeId)]
+sharedNodes [] ys = []
+sharedNodes xs [] = []
+sharedNodes l@(x@(id, (i1, _, _)):xs) r@(y@(_, (i2, _, _)):ys) 
+    | sameNode x y = (y, id):sharedNodes xs ys
+    | i1 < i2      = sharedNodes xs r
+    | otherwise    = sharedNodes l ys
+
+sameNode :: BDDNode -> BDDNode -> Bool
+sameNode (_, (a, b, c)) (_, (d, e, f)) = a == d && b == e && c == f
+
+deleteSubtrees :: [BDDNode] -> [(BDDNode, NodeId)] -> [BDDNode]
+deleteSubtrees [] xs = []
+deleteSubtrees xs [] = xs
+deleteSubtrees (x:xs) l@((y, _):ys) 
+    | x == y    = deleteSubtrees xs ys
+    | otherwise = x:deleteSubtrees xs l
+
+shareSubtrees:: [BDDNode] -> [(NodeId, NodeId)] -> [BDDNode]
+shareSubtrees xs [] = xs
+shareSubtrees [] ids = []
+shareSubtrees xs (id:ids) = 
+    if repeatedSubtree
+       then shareSubtrees (subtreesConcat zs [newNode]) ids
+       else shareSubtrees xs' ids
+    where repeatedSubtree = stillExists newNode zs
+          (xs', newNode) = linkNodes xs id
+          zs = delete newNode xs'
+
+linkNodes :: [BDDNode] -> (NodeId, NodeId) -> ([BDDNode], BDDNode)
+linkNodes (n@(id, (i, x, y)):ns) ids@(idx, idy)
+    | x == idx && y == idx = (a:ns, a)
+    | x == idx             = (b:ns, b)
+    | y == idx             = (c:ns, c) 
+    | otherwise            = (n:ms, m')
+    where a = (id, (i, idy, idy))
+          b = (id, (i, idy, y))
+          c = (id, (i, x, idy))
+          (ms, m') = linkNodes ns ids
+
+stillExists :: BDDNode -> [BDDNode] -> Bool
+stillExists x xs = not $ null $ sharedNodes [x] xs
+
 b1, b2, b3, b4, b5, b6, b7, b8 :: BExp
 b1 = Var False
 b2 = Not (And (IdRef 1) (Or (Var False) (IdRef 2)))
